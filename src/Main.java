9daa6a15d569a57a95e0075c12ff02050d0a7eca
GameDeveloperEpsilon <ryanchapman2084@gmail.com>
2023-03-06 19:04:18 -0600

    Documented procedure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Starting presentation!");
+
+//        Procedure
+//        1 User fills out form
+        System.out.println("Filling out form!");
+//        2 Form is saved to JSON
+        System.out.println("Translating form object to JSON!");
+//        3 JSON is saved to a file
+        System.out.println("Saving JSON string!");
+//        4 File is loaded
+        System.out.println("Loading file!");
+//        5 File is parsed
+        System.out.println("Parsing file string!");
+//        6 Display attributes
+        System.out.println("Displaying attributes!");
+
+        System.out.println("Ending presentation!");
     }
 }
